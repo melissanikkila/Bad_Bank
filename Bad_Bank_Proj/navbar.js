eaa64f012ae145479d7ce85d496d4be6f3506290
Melissa Nikkila <87152060+melissanikkila@users.noreply.github.com>
2022-01-11 10:24:44 -0600

    Update navbar.js

--- Bad_Bank_Proj/navbar.js
+++ Bad_Bank_Proj/navbar.js
@@ -3,64 +3,17 @@ function NavBar(){
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">BadBank</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-expand" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-            <a className="nav-link" href="#withdraw">Withdraw</a>
-            <a className="nav-link" href="#deposit">Deposit</a>
-            <a className="nav-link" href="#alldata">All Data</a>
-            <a className="nav-link" href="#createaccount">Create Account</a>
-            <a className="nav-link" href="#login">Login</a>
+            <a className="nav-link active" title="Click To Return Home" aria-current="page" href="#">Home</a>
+            <a className="nav-link"  title="Click To Withdraw" href="#withdraw">Withdraw</a>
+            <a className="nav-link" title="Click To Deposit" href="#deposit">Deposit</a>
+            <a className="nav-link" title="Click For Account Summary" href="#alldata">All Data</a>
+            <a className="nav-link" title="Click To Create Account" href="#createaccount">Create Account</a>
+            <a className="nav-link" title="Click To Log In" href="#login">Login</a>
           </div>
-          <form className="d-flex">
-      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-light" type="submit">Search</button>
-      </form>
         </div>
       </div>
     </nav>
     );
 }
-
-
-/* Original Code
- <ul className="nav nav-tabs">
-<li className="nav-item">
-  <Link className="nav-link" to="/">Home</Link>
-</li>
-<li className="nav-item">
-  <Link className="nav-link" to="/about/">About</Link>
-</li>
-<li className="nav-item">
-  <Link className="nav-link" to="/products">Products</Link>
-</li>
-</ul> */
-
-/* Edited
- <ul class="nav navbar-dark bg-dark">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#/">Home</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#balance">Balance</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#withdraw">Withdraw</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#deposit">Deposit</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#alldata">All Data</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#createaccount">Create Account</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#login">Login</a>
-  </li>
-  
-</ul> */
